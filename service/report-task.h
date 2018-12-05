2167c76f22edbdd5e07187c340b2cef092cf96ba
Ernestas Kulik <ekulik@redhat.com>
2018-12-05 13:54:07 +0100

    service: task: Drop g_type_class_add_private()
    Signed-off-by: Ernestas Kulik <ekulik@redhat.com>

--- service/report-task.h
+++ service/report-task.h
@@ -17,37 +17,17 @@
 #ifndef __REPORT_TASK_H__
 #define __REPORT_TASK_H__
 
-#include <giomm.h>
-
-#include "report-dbus-generated.h"
+#include <gio/gio.h>
 
 G_BEGIN_DECLS
 
-#define REPORT_TYPE_TASK            (report_task_get_type ())
-#define REPORT_TASK(inst)           (G_TYPE_CHECK_INSTANCE_CAST ((inst), REPORT_TYPE_TASK, ReportTask))
-#define REPORT_IS_TASK(inst)        (G_TYPE_CHECK_INSTANCE_TYPE ((inst), REPORT_TYPE_TASK))
-#define REPORT_TASK_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), REPORT_TYPE_TASK, ReportTaskClass))
-#define REPORT_IS_TASK_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), REPORT_TYPE_TASK))
-#define REPORT_TASK_GET_CLASS(obj)  (G_TYPE_INSTANCE_GET_CLASS ((obj), REPORT_TYPE_TASK, ReportTaskClass))
+#define REPORT_TYPE_TASK report_task_get_type ()
 
-typedef struct _ReportTask ReportTask;
-typedef struct _ReportTaskClass ReportTaskClass;
-typedef struct _ReportTaskPrivate ReportTaskPrivate;
+G_DECLARE_FINAL_TYPE(ReportTask, report_task, REPORT, TASK, GDBusObjectSkeleton)
 
-struct _ReportTask {
-	ReportDbusTaskSkeleton parent;
-	ReportTaskPrivate *pv;
-};
-
-struct _ReportTaskClass {
-	ReportDbusTaskSkeletonClass parent_class;
-};
-
-GType       report_task_get_type(void);
-
-ReportTask *report_task_new     (const gchar     *object_path,
-                                 const gchar     *problem_path,
-                                 struct workflow *workflow);
+ReportTask *report_task_new(const gchar     *object_path,
+                            const gchar     *problem_path,
+                            struct workflow *workflow);
 
 G_END_DECLS
 
